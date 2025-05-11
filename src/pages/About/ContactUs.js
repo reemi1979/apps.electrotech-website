0f2e4953f31420c443a58e6e2a37e75ce56305e9
reemi1979 <whemmy@outlook.com>
2025-05-11 16:02:26 -0400

    update

--- src/pages/About/ContactUs.js
+++ src/pages/About/ContactUs.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, MenuItem, TextField, Typography } from '@mui/material';
+import { Box, Button, Grid, MenuItem, TextField, Typography, Container  } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import validator from 'validator';
 
@@ -7,21 +7,15 @@ const ContactUs = () => {
   const { t } = useTranslation('about');
 
   const [formData, setFormData] = useState({
-    reason: '',
-    name: '',
-    contact: '',
-    company: '',
-    title: '',
-    country: '',
-    subject: '',
-    message: '',
+    reason: '', name: '', contact: '', company: '',
+    title: '', country: '', subject: '', message: ''
   });
 
   const [errors, setErrors] = useState({});
 
   const handleChange = (field) => (event) => {
     setFormData({ ...formData, [field]: event.target.value });
-    setErrors({ ...errors, [field]: '' }); // clear error on edit
+    setErrors({ ...errors, [field]: '' });
   };
 
   const validateFields = () => {
@@ -76,48 +70,93 @@ const ContactUs = () => {
   };
 
   return (
-    <Box sx={{ maxWidth: 600, mt: 10, mx: 'auto', p: 2 }}>
-      <Typography variant="h4" sx={{ mb: 3 }}>{t('contact_us_title')}</Typography>
 
-      <TextField
-        select fullWidth label={t('contact_us_reason')}
-        value={formData.reason} onChange={handleChange('reason')}
-        error={!!errors.reason} helperText={errors.reason} sx={{ mb: 2 }}
-      >
-        <MenuItem value="quotation">{t('contact_us_reason_quotation')}</MenuItem>
-        <MenuItem value="job">{t('contact_us_reason_job')}</MenuItem>
-        <MenuItem value="question">{t('contact_us_reason_question')}</MenuItem>
-        <MenuItem value="bug">{t('contact_us_reason_bug')}</MenuItem>
-        <MenuItem value="other">{t('contact_us_reason_other')}</MenuItem>
-      </TextField>
+<Container maxWidth={false} sx={{ maxWidth: '1280px', mx: 'auto', mt: 10, px: 2 }}>
+  <Grid container spacing={2}>
+    {/* LEFT COLUMN */}
+    <Box sx={{ maxWidth: 600, mx: 'auto', px: 2 }}>
+    <Grid item xs={12} md={6} >
+      
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          {t('contact_us_company_name')}
+        </Typography>
 
-      <TextField fullWidth label={t('contact_us_name')} value={formData.name}
-        onChange={handleChange('name')} error={!!errors.name} helperText={errors.name} sx={{ mb: 2 }} />
+        <Typography sx={{ whiteSpace: 'pre-line', mb: 3 }}>
+          {t('contact_us_address') + '\n\n' + t('contact_us_hours')}
+        </Typography>
 
-      <TextField fullWidth label={t('contact_us_contact')} value={formData.contact}
-        onChange={handleChange('contact')} error={!!errors.contact} helperText={errors.contact} sx={{ mb: 2 }} />
-
-      <TextField fullWidth label={t('contact_us_company')} value={formData.company}
-        onChange={handleChange('company')} sx={{ mb: 2 }} />
-
-      <TextField fullWidth label={t('contact_us_function')} value={formData.title}
-        onChange={handleChange('title')} sx={{ mb: 2 }} />
-
-      <TextField fullWidth label={t('contact_us_country')} value={formData.country}
-        onChange={handleChange('country')} sx={{ mb: 2 }} />
-
-      <TextField fullWidth label={t('contact_us_subject')} value={formData.subject}
-        onChange={handleChange('subject')} error={!!errors.subject} helperText={errors.subject} sx={{ mb: 2 }} />
-
-      <TextField fullWidth multiline rows={5} label={t('contact_us_message')}
-        value={formData.message} onChange={handleChange('message')}
-        error={!!errors.message} helperText={errors.message} sx={{ mb: 3 }} />
-
-      <Button variant="contained" color="primary" fullWidth onClick={handleSubmit}>
-        {t('contact_us_send')}
-      </Button>
+        <Box sx={{ height: { xs: 250, md: 530 } }}>
+          <iframe
+            title="Google Map"
+            width="100%"
+                style={{
+              border: 0,
+              height: '100%',
+            }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2770.1490664346265!2d-72.7279568237042!3d45.39018473927337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9ce4918ab93f1%3A0x5a408fc39d8aeeb9!2s625%20Rue%20Simonds%20S%2C%20Granby%2C%20QC%20J2J%201C2!5e0!3m2!1sfr!2sca!4v1715443842952!5m2!1sfr!2sca"
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </Box>
+     
+    </Grid>
     </Box>
+
+    {/* RIGHT COLUMN */}
+    <Box sx={{ maxWidth: 600, mx: 'auto', px: 2 }}>
+    <Grid item xs={12} md={6} >
+
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          {t('contact_us_title')}
+        </Typography>
+
+          <TextField select fullWidth label={t('contact_us_reason')}
+              value={formData.reason} onChange={handleChange('reason')}
+              error={!!errors.reason} helperText={errors.reason} sx={{ mb: 2 }}>
+              <MenuItem value="quotation">{t('contact_us_reason_quotation')}</MenuItem>
+              <MenuItem value="job">{t('contact_us_reason_job')}</MenuItem>
+              <MenuItem value="question">{t('contact_us_reason_question')}</MenuItem>
+              <MenuItem value="bug">{t('contact_us_reason_bug')}</MenuItem>
+              <MenuItem value="other">{t('contact_us_reason_other')}</MenuItem>
+            </TextField>
+
+            <TextField fullWidth label={t('contact_us_name')} value={formData.name}
+              onChange={handleChange('name')} error={!!errors.name} helperText={errors.name} sx={{ mb: 2 }} />
+
+            <TextField fullWidth label={t('contact_us_contact')} value={formData.contact}
+              onChange={handleChange('contact')} error={!!errors.contact} helperText={errors.contact} sx={{ mb: 2 }} />
+
+            <TextField fullWidth label={t('contact_us_company')} value={formData.company}
+              onChange={handleChange('company')} sx={{ mb: 2 }} />
+
+            <TextField fullWidth label={t('contact_us_function')} value={formData.title}
+              onChange={handleChange('title')} sx={{ mb: 2 }} />
+
+            <TextField fullWidth label={t('contact_us_country')} value={formData.country}
+              onChange={handleChange('country')} sx={{ mb: 2 }} />
+
+            <TextField fullWidth label={t('contact_us_subject')} value={formData.subject}
+              onChange={handleChange('subject')} error={!!errors.subject} helperText={errors.subject} sx={{ mb: 2 }} />
+
+            <TextField fullWidth multiline rows={5} label={t('contact_us_message')}
+              value={formData.message} onChange={handleChange('message')}
+              error={!!errors.message} helperText={errors.message} sx={{ mb: 3 }} />
+
+            <Button variant="contained" color="primary" fullWidth onClick={handleSubmit} sx={{ mb: 10 }}>
+              {t('contact_us_send')}
+            </Button>
+          
+    </Grid>
+    </Box>
+  </Grid>
+</Container>
+
+
+
   );
+
+  
 };
 
 export default ContactUs;
