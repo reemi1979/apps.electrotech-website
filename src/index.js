e87f86c3ebbbdb8d59e69e89479467199219c14a
reemi1979 <whemmy@outlook.com>
2025-05-14 20:37:48 -0400

    update

--- src/index.js
+++ src/index.js
@@ -23,4 +23,13 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals((metric) => {
+  if (window.gtag) {
+    window.gtag('event', metric.name, {
+      value: Math.round(metric.value),
+      event_category: 'Web Vitals',
+      event_label: metric.id,
+      non_interaction: true,
+    });
+  }
+});
